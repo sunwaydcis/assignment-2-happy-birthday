95364151534cb321ab854712bc80d303fb460808
Sum Hon You <sumhonyou@gmail.com>
2024-11-27 16:07:51 +0800

    First Version

--- app/Main.hs
+++ app/Main.hs
@@ -3,102 +3,147 @@ import Data.Function (on)
 import Data.List.Split (splitOn)
 import Data.Monoid
 import Data.Maybe (mapMaybe)
-import Control.Applicative (liftA2)
+import Text.Printf (printf)
+import Data.Ratio
+import Text.Read (readMaybe)
 
 
--- Define HospitalRecord for aggregation
+-- Define HospitalRecord data type
 data HospitalRecord = HospitalRecord
-    { state :: String
-    , totalBeds :: Sum Int
-    , covidBeds :: Sum Int
-    , admittedPUI :: Sum Int
-    , admittedCOVID :: Sum Int
-    , admittedTotal :: Sum Int
+    { date :: String
+    , state :: String
+    , totalBeds :: Int
+    , covidBeds :: Int
+    , noncritBeds :: Int
+    , admittedPUI :: Int
+    , admittedCOVID :: Int
+    , admittedTotal :: Int
+    , dischargedPUI :: Int
+    , dischargedCOVID :: Int
+    , dischargedTotal :: Int
+    , hospitalCOVID :: Int
+    , hospitalPUI :: Int
+    , hospitalNonCOVID :: Int
     } deriving Show
 
--- Semigroup and Monoid instances for aggregation
-instance Semigroup HospitalRecord where
-    r1 <> r2 = HospitalRecord
-        { state = state r1
-        , totalBeds = totalBeds r1 <> totalBeds r2
-        , covidBeds = covidBeds r1 <> covidBeds r2
-        , admittedPUI = admittedPUI r1 <> admittedPUI r2
-        , admittedCOVID = admittedCOVID r1 <> admittedCOVID r2
-        , admittedTotal = admittedTotal r1 <> admittedTotal r2
-        }
 
-instance Monoid HospitalRecord where
-    mempty = HospitalRecord "" mempty mempty mempty mempty mempty
+-- Helper function to safely read Int values
+readInt :: String -> Maybe Int
+readInt = readMaybe
+
+
+-- Function to find the index of a keyword in the header
+findHeaderIndex :: String -> [String] -> Maybe Int
+findHeaderIndex keyword headers = lookup keyword (zip headers [0..])
+
 
 -- Parse a single CSV row into HospitalRecord
-parseCSVLine :: String -> Either String HospitalRecord
-parseCSVLine line =
+parseCSVLine :: [String] -> String -> Either String HospitalRecord
+parseCSVLine headers line =
     let fields = splitOn "," line
-    in if length fields < 8
-        then Left $ "Invalid row: " ++ line
-        else Right $ HospitalRecord
-            (fields !! 1)  -- State
-            (Sum $ read (fields !! 2) :: Sum Int)  -- Total Bed
-            (Sum $ read (fields !! 3) :: Sum Int)  -- Total Bed Covid
-            (Sum $ read (fields !! 5) :: Sum Int)  -- Admitted PUI
-            (Sum $ read (fields !! 6) :: Sum Int)  -- Admitted COVID
-            (Sum $ read (fields !! 7) :: Sum Int)  -- Admitted Total
+        lookupField :: String -> Either String String
+        lookupField fieldName =
+            case findHeaderIndex fieldName headers of
+                Just idx -> if idx < length fields
+                            then Right (fields !! idx)
+                            else Left $ "Field " ++ fieldName ++ " not found in row"
+                Nothing -> Left $ "Field " ++ fieldName ++ " not found in header"
+    in do
+        dateVal          <- lookupField "date"
+        stateVal         <- lookupField "state"
+        totalBedsVal     <- lookupField "beds" >>= readIntEither
+        covidBedsVal     <- lookupField "beds_covid" >>= readIntEither
+        noncritBedsVal   <- lookupField "beds_noncrit" >>= readIntEither
+        admittedPUIVal   <- lookupField "admitted_pui" >>= readIntEither
+        admittedCOVIDVal <- lookupField "admitted_covid" >>= readIntEither
+        admittedTotalVal <- lookupField "admitted_total" >>= readIntEither
+        dischargedPUIVal <- lookupField "discharged_pui" >>= readIntEither
+        dischargedCOVIDVal <- lookupField "discharged_covid" >>= readIntEither
+        dischargedTotalVal <- lookupField "discharged_total" >>= readIntEither
+        hospitalCOVIDVal <- lookupField "hosp_covid" >>= readIntEither
+        hospitalPUIVal   <- lookupField "hosp_pui" >>= readIntEither
+        hospitalNonCOVIDVal <- lookupField "hosp_noncovid" >>= readIntEither
+        Right $ HospitalRecord dateVal stateVal totalBedsVal covidBedsVal noncritBedsVal
+                admittedPUIVal admittedCOVIDVal admittedTotalVal dischargedPUIVal dischargedCOVIDVal
+                dischargedTotalVal hospitalCOVIDVal hospitalPUIVal hospitalNonCOVIDVal
+  where
+    readIntEither :: String -> Either String Int
+    readIntEither str = maybe (Left $ "Invalid integer: " ++ str) Right (readInt str)
+
 
 -- Read and parse CSV file into HospitalRecords
 readCSV :: FilePath -> IO [HospitalRecord]
 readCSV filePath = do
     content <- lines <$> readFile filePath
-    let rows = tail content  -- Skip header
-    return $ mapMaybe (either (const Nothing) Just . parseCSVLine) rows
-
--- Group HospitalRecords by state and calculate averages;
-groupAndCalculateAverages :: [HospitalRecord] -> [(String, (Double, Double, Double))]
-groupAndCalculateAverages records =
-    let grouped = groupBy ((==) `on` state) $ sortBy (compare `on` state) records
-    in map calculateAverages grouped
+    let (header:rows) = content  -- First row is the header
+        headers = splitOn "," header
+    return $ mapMaybe (parseLine headers) rows
   where
-    calculateAverages :: [HospitalRecord] -> (String, (Double, Double, Double))
-    calculateAverages recordsForState =
-        let aggregated = mconcat recordsForState
-            totalRecords = fromIntegral $ length recordsForState  -- Convert to Double for division
-            avgPUI = fromIntegral (getSum (admittedPUI aggregated)) / totalRecords
-            avgCOVID = fromIntegral (getSum (admittedCOVID aggregated)) / totalRecords
-            avgTotal = fromIntegral (getSum (admittedTotal aggregated)) / totalRecords
-        in (state (head recordsForState), (avgPUI, avgCOVID, avgTotal))
+    parseLine headers line = case parseCSVLine headers line of
+        Left _ -> Nothing
+        Right valid -> Just valid
+
+
+-- Group records by state
+groupState :: [HospitalRecord] -> [[HospitalRecord]]
+groupState records =
+    let sortedRecords = sortBy (compare `on` state) records
+    in groupBy ((==) `on` state) sortedRecords
+
+
+-- Question 1: Find the state with the highest total beds
+maxBedsOfState :: [HospitalRecord] -> String
+maxBedsOfState records =
+    let maxBed = maximumBy (compare `on` totalBeds) records
+    in state maxBed
+
+
+-- Question 2: Calculate the ratio of COVID beds to total beds
+covidToTotalRatio :: [HospitalRecord] -> Ratio Int
+covidToTotalRatio records =
+    let totalCovidBeds = sum (map covidBeds records)
+        totalAllBeds = sum (map totalBeds records)
+    in fromIntegral totalCovidBeds % fromIntegral totalAllBeds
+   
+
+-- Question 3: Calculate averages of admissions by state
+averageAdmissions :: [HospitalRecord] -> [(String, (Double, Double))]
+averageAdmissions records =
+    let groupedRecords = groupState records
+    in map calculateAverages groupedRecords
+  where
+    calculateAverages group =
+        let stateName = state (head group)
+            totalRecords = length group
+            avgPUI = fromIntegral (sum (map admittedPUI group)) / fromIntegral totalRecords
+            avgCOVID = fromIntegral (sum (map admittedCOVID group)) / fromIntegral totalRecords
+        in (stateName, (avgPUI, avgCOVID))
+
 
 -- Main function
 main :: IO ()
 main = do
-    let filePath = "C:/Users/sumho/haskelprojectG1/Assignment2/app/hospital.csv"  -- Replace with your actual path
-    hospitalData <- readCSV filePath
+    -- readCSV "C:/Users/sumho/haskelprojectG1/Assignment2/app/hospital.csv"  >>= \records ->
 
-    --Question 2
-    -- Sum up all beds and beds_covid from each row
-    let totalCovidBeds = getSum (mconcat(map covidBeds hospitalData))
-        totalBeds' = getSum (mconcat(map totalBeds hospitalData))
+    --Q1 
+    -- putStrLn "\nQuestion 1: which state has the highest total of hospital bed"
+    -- (case maxBedsOfState records of
+    --     Just state -> putStrLn "\nState with maximum beds: \n" ++ state
+    --     Nothing -> putStrLn "Could not find the state")
+    let filePath = "C:/Users/sumho/haskelprojectG1/Assignment2/app/hospital.csv"
+    records <- readCSV filePath
 
-    -- Display total amount of beds for beds_covid and beds
-    putStrLn $ "Total Covid Beds: " ++ show totalCovidBeds
-    putStrLn $ "Total Beds: " ++ show totalBeds'
-    putStrLn $ "Total Covid Beds | Total Beds Ratio: "
-    putStrLn $ show totalCovidBeds ++ ":" ++ show totalBeds'
+    -- Question 1
+    let maxBeds = maxBedsOfState records
+    putStrLn $ "State with the highest total beds: " ++ maxBeds
 
+    -- Question 2
+    let ratio = covidToTotalRatio records
+    putStrLn $ "Ratio: " ++ show (numerator ratio) ++ ":" ++ show (denominator ratio)
+    printf "Ratio in decimal: %.2f\n" (fromRational (toRational ratio) :: Double)
 
-    --Question3
-    let averages = groupAndCalculateAverages hospitalData
-    
-    -- User interaction
-    putStrLn "Enter a command (e.g., 'Average of covid', 'Average of pui'):"
-    command <- getLine
-
-    case command of
-        "Average of covid" -> printAverages averages (\(_, avgCOVID, _) -> avgCOVID)
-        "Average of pui"   -> printAverages averages (\(avgPUI, _, _) -> avgPUI)
-        "Average of total" -> printAverages averages (\(_, _, avgTotal) -> avgTotal)
-        _ -> putStrLn "Unknown command. Please enter 'Average of covid', 'Average of pui', or 'Average of total'."
-            
-
-  where
-    printAverages :: [(String, (Double, Double, Double))] -> ((Double, Double, Double) -> Double) -> IO ()
-    printAverages averages selector =
-        mapM_ (\(stateName, avgTuple) -> putStrLn $ stateName ++ ": " ++ show (selector avgTuple)) averages
+    -- Question 3
+    let averages = averageAdmissions records
+    putStrLn "Average admissions by state (PUI, COVID, Total):"
+    mapM_ (\(stateName, (avgPUI, avgCOVID)) ->
+        putStrLn $ stateName ++ ": PUI=" ++ printf "%.2f" avgPUI ++ ", COVID=" ++ printf "%.2f" avgCOVID) averages
